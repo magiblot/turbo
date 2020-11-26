4dba39b3e96b71fc2543434fb047b47854a7611f
magiblot <magiblot@hotmail.com>
2020-11-26 22:50:08 +0100

    Generate intermediate library
    This makes it possible to link the editor classes against different 'main' functions very easily.

--- include/ScilexersPCH.h
+++ include/ScilexersPCH.h
@@ -5,13 +5,24 @@
 #include <assert.h>
 #include <ctype.h>
 
+#include <algorithm>
+#include <functional>
+#include <map>
+#include <set>
+#include <string>
+#include <vector>
+
 #include "ILexer.h"
 #include "Scintilla.h"
 #include "SciLexer.h"
 
 #include "WordList.h"
+#include "PropSetSimple.h"
 #include "LexAccessor.h"
 #include "Accessor.h"
 #include "StyleContext.h"
 #include "CharacterSet.h"
+#include "CharacterCategory.h"
 #include "LexerModule.h"
+#include "OptionSet.h"
+#include "DefaultLexer.h"
