magiblot/turbo

commit 82acc12596de0fb90c450552cae73520b51e3434

Implement uppercase/lowercase/capitalize case conversions
Closes #31.

source/turbo-core/tscintilla.cc
@@ -1,3 +1,6 @@
+#define Uses_TText
+#include <tvision/tv.h>
+
 #include <turbo/scintilla.h>
 #include <turbo/scintilla/tscintilla.h>
 #include <turbo/clipboard.h>
@@ -171,6 +174,61 @@ CaseFolder *TScintilla::CaseFolderForEncoding()
     if (IsUnicodeMode())
         return new CaseFolderUnicode();
     return super::CaseFolderForEncoding();
+}
+
+template <size_t (& next)(TStringView)>
+static std::string capitalize(TStringView s, const Scintilla::Document &doc)
+{
+    std::string result;
+    size_t i = 0;
+    while (i < s.size())
+    {
+        size_t spaceBegin = i;
+        while (i < s.size() && doc.WordCharacterClass(s[i]) != CharClassify::ccWord)
+            ++i;
+        auto space = s.substr(spaceBegin, i - spaceBegin);
+        result.append(space.data(), space.size());
+
+        size_t firstBegin = i;
+        i += next(s.substr(i));
+        auto first = s.substr(firstBegin, i - firstBegin);
+        result.append(CaseConvertString(first, CaseConversionUpper));
+
+        size_t tailBegin = i;
+        while (i < s.size() && doc.WordCharacterClass(s[i]) == CharClassify::ccWord)
+            ++i;
+        auto tail = s.substr(tailBegin, i - tailBegin);
+        result.append(CaseConvertString(tail, CaseConversionLower));
+    }
+    return result;
+}
+
+static size_t nextAscii(TStringView s)
+{
+    return max<size_t>(s.size(), 1);
+}
+
+std::string TScintilla::CaseMapString(const std::string &s, int mapping)
+{
+    if (IsUnicodeMode())
+        switch (turbo::CaseConversion(mapping))
+        {
+            case turbo::caseConvNone:
+                return s;
+            case turbo::caseConvUpper:
+                return CaseConvertString(s, CaseConversionUpper);
+            case turbo::caseConvLower:
+                return CaseConvertString(s, CaseConversionLower);
+            case turbo::caseConvCapitalize:
+                return capitalize<TText::next>(s, *pdoc);
+        }
+    switch (turbo::CaseConversion(mapping))
+    {
+        case turbo::caseConvCapitalize:
+            return capitalize<nextAscii>(s, *pdoc);
+        default:
+            return super::CaseMapString(s, mapping);
+    }
 }
 
 int TScintilla::KeyDefault(int key, int modifiers) {

include/turbo/editstates.h
@@ -126,6 +126,21 @@ inline bool ThemingState::hasLexer() const
 void stripTrailingSpaces(TScintilla &scintilla);
 void ensureNewlineAtEnd(TScintilla &scintilla);
 
+inline void uppercase(TScintilla &scintilla)
+{
+    changeCaseOfSelection(scintilla, caseConvUpper);
+}
+
+inline void lowercase(TScintilla &scintilla)
+{
+    changeCaseOfSelection(scintilla, caseConvLower);
+}
+
+inline void capitalize(TScintilla &scintilla)
+{
+    changeCaseOfSelection(scintilla, caseConvCapitalize);
+}
+
 } // namespace turbo
 
 #endif

include/turbo/scintilla.h
@@ -51,6 +51,16 @@ TColorAttr getStyleColor(TScintilla &, int style);
 void setSelectionColor(TScintilla &, TColorAttr attr);
 void setWhitespaceColor(TScintilla &, TColorAttr attr);
 
+enum CaseConversion
+{
+    caseConvNone,
+    caseConvUpper,
+    caseConvLower,
+    caseConvCapitalize,
+};
+
+void changeCaseOfSelection(TScintilla &, CaseConversion);
+
 class TScintillaParent
 {
 public:

include/turbo/scintilla/tscintilla.h
@@ -42,6 +42,7 @@ protected:
     void AddToPopUp(const char *label, int cmd=0, bool enabled=true) override;
 
     CaseFolder *CaseFolderForEncoding() override;
+    std::string CaseMapString(const std::string &, int) override;
     int KeyDefault(int key, int modifiers) override;
 
 public:
@@ -63,6 +64,7 @@ public:
     using super::Paint;
     using super::pasteStream;
     using super::CharacterSource;
+    using super::ChangeCaseOfSelection;
 
 };
 

source/turbo-core/scintilla.cc
@@ -235,4 +235,9 @@ void setWhitespaceColor(TScintilla &self, TColorAttr attr)
     call(self, SCI_SETWHITESPACEBACK, !bg.isDefault(), convertColor(bg).AsInteger());
 }
 
+void changeCaseOfSelection(TScintilla &self, CaseConversion cnv)
+{
+    self.ChangeCaseOfSelection(cnv);
+}
+
 } // namespace turbo

source/turbo/app.cc
@@ -63,6 +63,9 @@ TurboApp::TurboApp(int argc, const char *argv[]) noexcept :
     ts += cmSearchPrev;
     ts += cmToggleIndent;
     ts += cmCloseEditor;
+    ts += cmSelUppercase;
+    ts += cmSelLowercase;
+    ts += cmSelCapitalize;
     disableCommands(ts);
 
     // Actions that only make sense when there is at least one editor.
@@ -127,6 +130,10 @@ TMenuBar *TurboApp::initMenuBar(TRect r)
             *new TMenuItem( "~R~eplace...",cmReplace, kbCtrlR, hcNoContext, "Ctrl-R" ) +
             *new TMenuItem( "Find ~N~ext", cmSearchAgain, kbF3, hcNoContext, "F3" ) +
             *new TMenuItem( "Find ~P~revious", cmSearchPrev, kbShiftF3, hcNoContext, "Shift-F3" ) +
+        *new TSubMenu( "Se~l~ection", kbAltL ) +
+            *new TMenuItem( "~U~ppercase", cmSelUppercase, kbNoKey, hcNoContext ) +
+            *new TMenuItem( "~L~owercase", cmSelLowercase, kbNoKey, hcNoContext ) +
+            *new TMenuItem( "~C~apitalize", cmSelCapitalize, kbNoKey, hcNoContext ) +
         *new TSubMenu( "~W~indows", kbAltW ) +
             *new TMenuItem( "~Z~oom", cmZoom, kbF5, hcNoContext, "F5" ) +
             *new TMenuItem( "~R~esize/move",cmResize, kbCtrlF5, hcNoContext, "Ctrl-F5" ) +

source/turbo/app.h
@@ -24,7 +24,10 @@ const ushort
     cmTreeNext      = 107,
     cmTreePrev      = 108,
     cmCloseEditor   = 109,
-    cmRename        = 110;
+    cmRename        = 110,
+    cmSelUppercase  = 111,
+    cmSelLowercase  = 112,
+    cmSelCapitalize = 113;
 
 // Commands that cannot be deactivated.
 const ushort

source/turbo/editwindow.cc
@@ -34,6 +34,9 @@ EditorWindow::EditorWindow( const TRect &bounds, TurboEditor &aEditor,
     enabledCmds += cmSearchPrev;
     enabledCmds += cmToggleIndent;
     enabledCmds += cmCloseEditor;
+    enabledCmds += cmSelUppercase;
+    enabledCmds += cmSelLowercase;
+    enabledCmds += cmSelCapitalize;
 
     // Commands that always get disabled when unfocusing the editor.
     disabledCmds += enabledCmds;
@@ -74,6 +77,18 @@ void EditorWindow::handleEvent(TEvent &ev) {
                 break;
             case cmCloseEditor:
                 handled = message(this, evCommand, cmClose, nullptr); // May delete 'this'!
+                break;
+            case cmSelUppercase:
+                turbo::uppercase(editor.scintilla);
+                editor.partialRedraw();
+                break;
+            case cmSelLowercase:
+                turbo::lowercase(editor.scintilla);
+                editor.partialRedraw();
+                break;
+            case cmSelCapitalize:
+                turbo::capitalize(editor.scintilla);
+                editor.partialRedraw();
                 break;
             default:
                 handled = false;

commit fab4833d52fc12b75cff88dc5513a778de34f647

Fix bug in ASCII text capitalization
Introduced in 82acc12596de0fb90c450552cae73520b51e3434.

source/turbo-core/tscintilla.cc
@@ -210,7 +210,7 @@ static size_t nextUnicode(TStringView s) noexcept
 
 static size_t nextAscii(TStringView s) noexcept
 {
-    return max<size_t>(s.size(), 1);
+    return min<size_t>(s.size(), 1);
 }
 
 std::string TScintilla::CaseMapString(const std::string &s, int mapping)